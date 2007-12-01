ccbc372b7f247d39d1512514851f80d46bd5f448
Shawn M Moore <sartak@gmail.com>
2007-12-01 03:22:07 -0500

    Add a Sequential solver

---
+++ lib/Games/Mastermind/Solver/Sequential.pm
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+package Games::Mastermind::Solver::Sequential;
+use Moose;
+extends 'Games::Mastermind::Solver';
+
+sub make_guess {
+    my $self = shift;
+    my $last_guess = $self->last_guess;
+
+    return $self->pegs->[0] x $self->holes
+        if !defined($last_guess);
+
+    return $self->increment_guess($last_guess);
+}
+
+sub increment_guess {
+    my $self  = shift;
+    my $guess = shift;
+
+    # map peg to number
+    my $pegs = 0;
+    my %number_of = map { $_ => ++$pegs } @{ $self->pegs };
+
+    # convert the guess to an array of numbers
+    my @guess = map { $number_of{$_} } split '', $guess;
+
+    # increment guess, return undef if we're at the top guess
+    $guess[-1]++;
+    for (my $i = @guess - 1; $i >= 0; --$i) {
+        if ($guess[$i] > $pegs) {
+            return undef if $i == 0; # top
+            $guess[$i-1]++;
+            $guess[$i] = 1;
+        }
+        else {
+            last;
+        }
+    }
+
+    return join '', map { $self->pegs->[$_-1] } @guess;
+}
+
+1;
+
