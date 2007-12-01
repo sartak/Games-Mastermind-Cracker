5df198e9a82f26b1dd08397f05e39f45e2a223e5
Shawn M Moore <sartak@gmail.com>
2007-12-01 02:18:00 -0500

    Initial import of Games::Mastermind::Solver

---
+++ lib/Games/Mastermind/Solver.pm
@@ -0,0 +1,83 @@
+#!perl
+package Games::Mastermind::Solver;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+Games::Mastermind::Solver - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Games::Mastermind::Solver;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-games-mastermind-solver at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-Mastermind-Solver>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Games::Mastermind::Solver
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Games-Mastermind-Solver>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Games-Mastermind-Solver>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-Mastermind-Solver>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Games-Mastermind-Solver>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
