c971549c315978c14ea852a93407a6cad0ea6cd6
Shawn M Moore <sartak@gmail.com>
2007-12-01 05:01:20 -0500

    Add an elimination role that handles the first part of Knuth's algorithm

---
+++ lib/Games/Mastermind/Solver/Role/Elimination.pm
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+package Games::Mastermind::Solver::Role::Elimination;
+use Moose::Role;
+
+has possibilities => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        $self->all_codes;
+    },
+);
+
+around result_of => sub {
+    my $orig = shift;
+    my ($self, $guess, $black, $white) = @_;
+
+    # reset iterator
+    keys %{ $self->possibilities };
+
+    while (my $code = each %{ $self->possibilities }) {
+        my ($b, $w) = $self->score($guess, $code);
+        delete $self->possibilities->{$code}
+            unless $b == $black && $w == $white;
+    }
+
+    $orig->(@_);
+};
+
+around make_guess => sub {
+    my $orig = shift;
+    my ($self) = @_;
+
+    if (keys %{ $self->possibilities } == 1) {
+        my ($correct) = keys %{ $self->possibilities };
+        return \$correct;
+    }
+
+    $orig->(@_);
+}
+
+1;
+
